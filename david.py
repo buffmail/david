cf5dd9e87442c870cea1a72af9f449ff7bde242a
Chang Jaehwa <buffmail@gmail.com>
2015-10-12 07:20:56 +0900

    extract func for open url.

--- david.py
+++ david.py
@@ -7,25 +7,27 @@ from Tkinter import Tk
 ENDIC_URL = 'http://m.endic.naver.com/search.nhn?query={}&searchOption='
 DIC_URL = 'http://dictionary.reference.com/browse/{}?s=t'
 
-if __name__ == '__main__':
+def openDic(keyword):
+    for url in (ENDIC_URL, DIC_URL):
+        webbrowser.open(url.format(keyword))
 
+if __name__ == '__main__':
     if len(sys.argv) == 2:
         keyword = sys.argv[1]
-        for url in (ENDIC_URL, DIC_URL):
-            webbrowser.open(url.format(keyword))
+        openDic(keyword)
         os.exit(0)
+
+    text = None
 
     r = Tk()
     r.withdraw()
-
-    text = ''
 
     while True:
         result = r.selection_get(selection="CLIPBOARD")
         newText = result.split('\n')[0].strip(".")
         if newText != text:
-            for url in (ENDIC_URL, DIC_URL):
-                webbrowser.open(url.format(newText))
+            openDic(newText)
             text = newText
         time.sleep(1)
+
     r.destroy()

e305dffa20f9f36e1ea71a3952c04cde50edcd2c
Chang Jaehwa <buffmail@gmail.com>
2015-10-14 11:02:27 +0900

    remove newline (kindle annotation) from clipboard

--- david.py
+++ david.py
@@ -8,14 +8,15 @@ from Tkinter import Tk
 ENDIC_URL = 'http://m.endic.naver.com/search.nhn?query={}&searchOption='
 DIC_URL = 'http://dictionary.reference.com/browse/{}?s=t'
 
-def openDic(keyword):
+def openDic(rawKeyword):
+    keyword = rawKeyword.split('\n')[0].strip(".")
     logging.info('keyword [{}]'.format(keyword))
     for url in (ENDIC_URL, DIC_URL):
         webbrowser.open(url.format(keyword))
 
 def getClipboard(tk):
     result = r.selection_get(selection="CLIPBOARD")
-    return result.split('\n')[0].strip(".")
+    return result
 
 if __name__ == '__main__':
     dir = os.path.dirname(os.path.realpath(__file__))

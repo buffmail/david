e899454f7bb2384fc1059d2c405f1e78289fcecc
Chang Jaehwa <buffmail@gmail.com>
2016-03-13 14:29:10 +0900

    navigate between days

--- davidserver.js
+++ davidserver.js
@@ -7,12 +7,11 @@ const PORT=8080;
 const mainHtmlPath = __dirname + "/david.html";
 console.log(mainHtmlPath);
 
-var jsonObj = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+var jsonObjs = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 
 var server = http.createServer(function(request, response){
     var path = url.parse(request.url).pathname;
-
-    console.log(path);
+    console.log('GET ' + path);
 
     switch(path){
     case '/':
@@ -40,14 +39,39 @@ server.listen(PORT);
 
 var io2 = io.listen(server);
 
+var fnGetDay = function(dayOffset){
+    var d = new Date();
+    d.setDate(d.getDate() + dayOffset);
+    return Math.floor(d.getTime() / (1000 * 60 * 60 * 24));
+}
+
+var fnGetDayJson = function(jsonObjs, day){
+    if (jsonObjs.length == 0){
+        return {}
+    }
+
+    for (var i in jsonObjs)
+    {
+        var jsonObj = jsonObjs[i];
+        var d = new Date(jsonObj.date);
+        var days = Math.floor(d.getTime() / (1000 * 60 * 60 * 24));
+        if (days == day){
+            return jsonObj;
+        }
+    }
+    return jsonObjs[jsonObjs.length - 1];
+}
+
 io2.sockets.on('connection', function(socket){
     var connId = socket.conn.id;
     console.log("connected " + connId);
     socket.on('disconnection', function(){
         console.log("disconnected " + connId);
     });
-    socket.on('register', function(data){
-        console.log('register req');
-        socket.emit('onReadExamples', jsonObj);
+    socket.on('req_example', function(data){
+        var day = fnGetDay(data.day_offset);
+        var jsonObj = fnGetDayJson(jsonObjs, day);
+        jsonObj.day_offset = data.day_offset;
+        socket.emit('res_example', jsonObj);
     });
 });

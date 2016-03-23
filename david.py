d87f52c2c3e2f403b8f39bac2a4f510e575d9890
Chang Jaehwa <buffmail@gmail.com>
2016-03-23 22:28:04 +0900

    adjust browser pos as opposite side of cursor.

--- david.py
+++ david.py
@@ -3,6 +3,8 @@ import sys
 import time
 import webbrowser
 import logging
+import win32gui
+import win32api
 from Tkinter import Tk
 
 ENDIC_URL = 'http://m.endic.naver.com/search.nhn?query={}&searchOption='
@@ -14,19 +16,37 @@ def openDic(rawKeyword):
     for url in (DIC_URL, ENDIC_URL):
         webbrowser.open(url.format(keyword))
 
+def adjustWinPos():
+    adjustWinPos.chromeWin = None
+
+    def callback(hwnd, name):
+        if not win32gui.IsWindowVisible(hwnd):
+            return
+        className = win32gui.GetClassName(hwnd)
+        if className == name:
+            logging.info('found {} hwnd {}'.format(name, hwnd))
+            adjustWinPos.chromeWin = hwnd
+
+    win32gui.EnumWindows(callback, 'Chrome_WidgetWin_1')
+
+    if adjustWinPos.chromeWin:
+        hw = win32api.GetSystemMetrics(0) / 2
+        height = win32api.GetSystemMetrics(1)
+        cursorX, _ = win32api.GetCursorPos()
+        xPos = (cursorX < hw) and 0 or hw
+        logging.info('cursorX {} hw {} pos {}'.format(cursorX, hw, xPos))
+        win32gui.MoveWindow(adjustWinPos.chromeWin,
+                            xPos, 0, hw, height, True)
+
 def getClipboard(tk):
     result = r.selection_get(selection="CLIPBOARD")
     return result
 
-if __name__ == '__main__':
-    dir = os.path.dirname(os.path.realpath(__file__))
-    logPath = dir + '/david.log'
-
-    logging.basicConfig(filename=logPath, level=logging.INFO)
-    logging.info('started with arg {}'.format(sys.argv))
+def main():
     if (len(sys.argv) > 1):
         keyword = ' '.join(sys.argv[1:])
         openDic(keyword)
+        adjustWinPos()
         sys.exit(0)
 
     r = Tk()
@@ -38,7 +58,20 @@ if __name__ == '__main__':
         newText = getClipboard(r)
         if newText != text:
             openDic(newText)
+            adjustWinPos()
             text = newText
         time.sleep(1)
 
     r.destroy()
+
+if __name__ == '__main__':
+    dir = os.path.dirname(os.path.realpath(__file__))
+    logPath = dir + '/david.log'
+
+    logging.basicConfig(filename=logPath, level=logging.INFO)
+    logging.info('started with arg {}'.format(sys.argv))
+
+    try:
+        main()
+    except Exception as e:
+        logging.warn('{}', e)
